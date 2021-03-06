8989fafbb80ed8408996cc718d1ee8d444771d42
Adem <tpegrp6132@gmail.com>
2021-03-06 10:20:13 +0100

    projet fini 2

--- backend/connection.js
+++ backend/connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
+require('dotenv').config()
 
 const db = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'adem',
-    password : 'adem13200',
-    database : 'groupomania'
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_BASE
 });
 
 module.exports = db; 
